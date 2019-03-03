7f5ab3d6e82616d7c87c1da89f687b76947d197d
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-03 22:44:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,12 +8,12 @@ class Dog
     @name
   end
   
-   def name=(dogs_name)
-    @name = dogs_name
+   def breed=(dogs_breed)
+    @breed = dogs_breed
   end
  
-  def name
-    @name
+  def breed
+    @breed
   end
   
 end
